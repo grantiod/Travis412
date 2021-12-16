68ae112c516cb51827ea83082635bf1782b8a706
grantiod <giodice1212@gmail.com>
2021-12-16 14:45:39 -0500

    yeehaw

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -10,5 +10,26 @@ public class SimpleCalculatorTest {
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
+		assertEquals(calc.add(2, 0), 2);
+	}
+	
+	@Test
+	public void testMinus() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.minus(5, 3), 2);
+	}
+	
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(5, 3), 15);
+		assertEquals(calc.multiply(5, 0), 0);
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(5, 2), 1.5);
 	}
 }
